970bf3290e3e2cb5cd462326521aaca6084a3801
AbigailLevi <Filsaimeam@gmail.com>
2022-05-18 18:27:01 -0400

    Updates

---
+++ precache-manifest.8b4aa5153e396e08f0f108c2833880cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed5cc3c1bfb9d2ba73f3d159b28be33",
+    "url": "/Breathe.flowin/index.html"
+  },
+  {
+    "revision": "78afe19981dc6b2c42a6",
+    "url": "/Breathe.flowin/static/css/main.ddbd203f.chunk.css"
+  },
+  {
+    "revision": "492bdceff23f843db8a4",
+    "url": "/Breathe.flowin/static/js/2.bca56110.chunk.js"
+  },
+  {
+    "revision": "308d2d97fd519422d20953c3e51f94e3",
+    "url": "/Breathe.flowin/static/js/2.bca56110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78afe19981dc6b2c42a6",
+    "url": "/Breathe.flowin/static/js/main.20ccc3d6.chunk.js"
+  },
+  {
+    "revision": "8043d64245987b5d8ece",
+    "url": "/Breathe.flowin/static/js/runtime-main.630f6bdf.js"
+  },
+  {
+    "revision": "a2becd089e066602ca9022a6b6a3f465",
+    "url": "/Breathe.flowin/static/media/kendrickawake.a2becd08.webp"
+  }
+]);
